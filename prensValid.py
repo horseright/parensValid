3e3a767e9107a4a09829caa350975d51d3113662
wry232 <amywang200@gmail.com>
2016-10-05 11:37:19 -0700

    Add files via upload

---
+++ prensValid.py
@@ -0,0 +1,17 @@
+myStr = 'ab(cde)ef)'
+
+def parensValid(Str):
+	count =0
+	for i in range (len(Str)):
+		if (count<0):
+			return False
+		elif (Str[i]=='('):
+			count =count+1
+		elif (Str[i]==')'):
+			count=count-1
+	if (count!=0):
+		return False
+	else:
+		return True
+
+print parensValid(myStr)
